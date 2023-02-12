c28b44f7942474064f3eaf332a35c0816b06d246
Tejas Ravishankar <tejas.ravishankar02@gmail.com>
2023-02-12 01:42:03 -0800

    changed backgorund and server link

--- client/src/api/machines.js
+++ client/src/api/machines.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://muir-laundry-app-api.onrender.com";
+const URL = "https://muir-laundry-app-api.onrender.com/machines";
 
 export const getMachines = () => axios.get(URL);
 export const updateMachine = (id, updatedMachine) =>
